2218b3f13f317ee013872ceb6712b210444c8e8a
Filipe Cândido <filipecandido123@hotmail.com>
2022-10-06 17:50:43 -0300

    refactor: crypto-adapter change name function

--- src/data/protocols/criptography/crypto.ts
+++ src/data/protocols/criptography/crypto.ts
@@ -1,3 +1,3 @@
 export interface Crypto {
-  randomBytes: () => Promise<string>
+  randomUUID: () => Promise<string>
 }
--- src/infra/criptography/crypto-adapter/crypto-adapter.ts
+++ src/infra/criptography/crypto-adapter/crypto-adapter.ts
@@ -2,8 +2,7 @@ import { Crypto } from '../../../data/protocols/criptography/crypto'
 import crypto from 'crypto'
 
 export class CryptoAdapter implements Crypto {
-  async randomBytes(): Promise<string> {
-    const hash = await crypto.randomUUID()
-    return hash
+  async randomUUID(): Promise<string> {
+    return crypto.randomUUID()
   }
 }
